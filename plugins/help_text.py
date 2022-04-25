713f7d9565dc09315c263595381a4c09e58094b5
hasithxd <104183587+hasithxd@users.noreply.github.com>
2022-04-25 10:21:58 +0530

    Add files via upload

---
+++ plugins/help_text.py
@@ -0,0 +1,46 @@
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+"""
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+"""
+from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        reply_to_message_id=update.message_id
+    )
+
